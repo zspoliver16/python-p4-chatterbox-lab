dbe80330ce3acdf76fd77319030dca858b6eed86
zachary oliver <zspoliver@gmail.com>
2024-06-10 12:36:27 -0400

    adds chatterbox

--- server/app.py
+++ server/app.py
@@ -14,13 +14,37 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.order_by(Message.created_at).all():
+            messages.append(message.to_dict())
+        return make_response(messages)
+    elif request.method == 'POST':
+        params = request.json
+        new_message = Message(body=params['body'], username=params['username'])
 
-@app.route('/messages/<int:id>')
+        db.session.add(new_message)
+        db.session.commit()
+
+        return make_response(new_message.to_dict(), 201)
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get(id)
+    if message:
+        if request.method == 'PATCH':
+            params = request.json
+            # for attr in params:
+            #     setattr(message, attr, params[attr])
+            message.body = params['body']
+            db.session.commit()
+            return make_response(message.to_dict())
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+            return make_response('', 204)
 
 if __name__ == '__main__':
     app.run(port=5555)
